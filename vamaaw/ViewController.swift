ec6d5b09a931cb3513ce7e9f5aa36ece677e59c6
Andrew Malmstead <malmsteadaa@gmail.com>
2022-02-24 00:44:29 -0500

    Initial Commit

---
+++ vamaaw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vamaaw
+//
+//  Created by admin on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
