fb5ffbbc5154fba803812233eaca677149b96e29
BellJ13 <joseph.m.bell111@gmail.com>
2022-02-28 17:54:33 -0500

    jygyhj

--- vamaaw/ViewController.swift
+++ vamaaw/ViewController.swift
@@ -4,11 +4,13 @@
 //
 //  Created by admin on 2/24/22.
 //
+//rger
 //you all ready for this ?
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gymReview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
