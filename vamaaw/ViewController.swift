d5504d97e43e5fe205f35fbd4026c851faaaf66f
BellJ13 <joseph.m.bell111@gmail.com>
2022-03-02 17:38:50 -0500

    Table view and navigation

--- vamaaw/ViewController.swift
+++ vamaaw/ViewController.swift
@@ -8,9 +8,47 @@
 //you all ready for this ?
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var gymReview: UITableView!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "gymCell") as! UserTableViewCell
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+       return 4
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section{
+        case 0:
+            return "How was your Gym experience?"
+        case 1:
+            return "Was there a variety of equipment?"
+        case 2:
+            return "How did the staff treat you?"
+        case 3:
+            return "Was the equpiment clean?"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            switch indexPath.item {
+            case 0 :
+                return print("happy")
+            case 1:
+                return print("ok")
+            case 2:
+                return print("bad")
+            default:
+                return print("")
+            }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
