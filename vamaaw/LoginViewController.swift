046f7371ae2015d5dcb6940c836729e40cb6860d
bome123161 <gun123161@gmail.com>
2022-03-07 12:44:51 -0500

    add login feature via key chain and gave a type to name for bug fixing

---
+++ vamaaw/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  vamaaw
+//
+//  Created by admin on 3/7/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var pw: UITextField!
+    @IBOutlet weak var un: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: Any) {
+        let q:[String : Any] = [kSecClass as String : kSecClassGenericPassword, kSecAttrAccount as String : un.text, kSecReturnAttributes as String : true, kSecReturnData as String : true]
+        var res : CFTypeRef?
+        if SecItemCopyMatching(q as CFDictionary, &res) == noErr{
+         if   let item = res as? [String : Any],
+            let uid = item[kSecAttrAccount as String] as? String,
+            let password = item [ kSecValueData as String] as? Data,
+            let pass = String(data: password, encoding: .utf8){
+                print("id is", uid, "password is", pass)
+             if(pass==pw.text!){
+                 success()
+             }else{
+                 print("failed login")
+             }
+            }
+        }else{
+            print("no data found")
+        }
+    }
+    func success(){
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
