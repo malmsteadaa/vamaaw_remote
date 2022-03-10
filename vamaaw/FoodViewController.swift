4ab02217f6a2df23a7d2d2c118d96f1ecf45a578
BellJ13 <joseph.m.bell111@gmail.com>
2022-03-09 21:42:26 -0500

    commit

--- vamaaw/FoodViewController.swift
+++ vamaaw/FoodViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class FoodViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    //placeholder faces
-    var imgData = ["img","midimg","sadimg"]
-    var foodTotal = 0
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
@@ -19,75 +17,23 @@ class FoodViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "foodCell") as! UserTableViewCell
-        
-        switch indexPath.section{
-        case 0:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 1:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 2:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 3:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        default:
-            return cell
-        }
-        
-    
+        return cell
         
         }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-       return 4
+       return 1
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section{
-        case 0:
-            return "How was the food?"
-        case 1:
-            return "Was the food prepared in a timely manner?"
-        case 2:
-            return "How did the staff treat you?"
-        case 3:
-            return "How clean were the utensils?"
-        default:
-            return ""
-        }
+        return "Your Food Reviews"
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                switch indexPath.item {
-                case 0 :
-                    foodTotal = 3 + foodTotal
-                    return print(foodTotal)
-                case 1:
-                    foodTotal = 1 + foodTotal
-                    return print(foodTotal)
-                case 2:
-                    foodTotal = 0 + foodTotal
-                    return print(foodTotal)
-                default:
-                    return print(foodTotal)
-                }
+              
     }
-    var result : Int!
     
-    @IBAction func submitReview(_ sender: Any) {
-        result = foodTotal
-        if foodTotal <= 6 {
-            performSegue(withIdentifier: "gotobadReview", sender: self)
-            foodTotal = 0
-        }
-        else{
-            performSegue(withIdentifier: "gotogoodReview", sender: self)
-            foodTotal = 0
-        }
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -95,4 +41,3 @@ class FoodViewController: UIViewController, UITableViewDelegate, UITableViewData
 
 
 }
-
--- vamaaw/RoomViewController.swift
+++ vamaaw/RoomViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class RoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    //placeholder faces
-    var imgData = ["img","midimg","sadimg"]
-    var roomTotal = 0
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
@@ -19,74 +17,23 @@ class RoomViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "roomCell") as! UserTableViewCell
-        
-        switch indexPath.section{
-        case 0:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 1:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 2:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        case 3:
-            cell.face.image = UIImage(named: imgData[indexPath.row])
-            return cell
-        default:
-            return cell
-        }
-        
-    
+        return cell
         
         }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-       return 4
+       return 1
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section{
-        case 0:
-            return "How was your Room?"
-        case 1:
-            return "Was there enough space in your room?"
-        case 2:
-            return "How did the staff treat you?"
-        case 3:
-            return "Was the room cleaned in a timely manner?"
-        default:
-            return ""
-        }
+        return "Your Room Reviews"
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.item {
-        case 0 :
-            roomTotal = 3 + roomTotal
-            return print(roomTotal)
-        case 1:
-            roomTotal = 1 + roomTotal
-            return print(roomTotal)
-        case 2:
-            roomTotal = 0 + roomTotal
-            return print(roomTotal)
-        default:
-            return print(roomTotal)
-        }
+              
     }
-    var result : Int!
-    @IBAction func submitReview(_ sender: Any) {
-        result = roomTotal
-        if roomTotal <= 6 {
-            performSegue(withIdentifier: "gotobadReview", sender: self)
-            roomTotal = 0
-        }
-        else{
-            performSegue(withIdentifier: "gotogoodReview", sender: self)
-            roomTotal = 0
-        }
-    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -94,5 +41,3 @@ class RoomViewController: UIViewController, UITableViewDelegate, UITableViewData
 
 
 }
-
-
